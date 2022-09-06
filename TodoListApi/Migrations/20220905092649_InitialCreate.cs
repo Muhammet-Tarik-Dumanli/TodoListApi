2e918e91b6e9a44b5c2f8760e9c37b77b78125b1
MuhammetTarik <mtdumanli@gmail.com>
2022-09-06 12:28:25 +0300

    Add project files.

---
+++ TodoListApi/Migrations/20220905092649_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoListApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDone = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItems",
+                columns: new[] { "Id", "IsDone", "Title" },
+                values: new object[,]
+                {
+                    { 1, false, "Go shopping" },
+                    { 2, false, "Do your homework" },
+                    { 3, true, "Wash your car" },
+                    { 4, true, "Watch Netflix" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
